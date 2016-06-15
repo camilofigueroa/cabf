5a9e73f7b4993a539a7dd2b7e0613f301b0a36db
obocanegra <oabs03@gmail.com>
2016-06-15 18:14:06 -0430

    Add files via upload

---
+++ www/login.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <link rel="stylesheet" href="css/cabf.css" >
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <title>Administrador</title>
+    </head>
+    <body>
+        <div id="container" class="container">
+            <div class="row">
+            	<div class="col-md-1"></div>
+                <div class="col-md-10" align="center">
+                	<img src="imagenes/banner3.png" class="img-responsive" />
+                </div>
+                <div class="col-md-1"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-10" align="center">
+                    <div class="row">
+                        <div class="col-md-3"></div>
+                        <div class="col-md-6" align="center">
+	                     <form class="flogin ensayo">
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-addon" ><img src="iconos/usuario.png"  style="width:20px;"></div>
+                                        <input type="text" class="form-control" id="inputusuario" placeholder="Usuario">
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <div class="input-group">
+                                        <div class="input-group-addon"><img src="iconos/contraseña.png"  style="width:20px;"></div>
+                                        <input type="text" class="form-control" id="inputusuario" placeholder="Contraseña">
+                                    </div>
+                                </div>
+                                 <button class="btn pull-right" id="uno">Iniciar</button>
+							</form> 
+                         </div>
+                        <div class="col-md-3"></div>
+                    </div>                 
+                </div>
+                <div class="col-md-1"></div>
+            </div>
+            <div class="row">
+                <div class="col-md-1"></div>
+                <div class="col-md-10 " align="center">
+                    <img src="imagenes/banner4.png" class="img-responsive" />
+                </div>
+                <div class="col-md-1"></div>
+            </div>
+        </div>
+    </body>
+</html>
