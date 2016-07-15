69ca8986379bdbd0ff637b645f016195ba690ce2
obocanegra <oabs03@gmail.com>
2016-07-15 18:42:03 -0400

    mejoras login
    se realizaron validación del login

--- www/login.php
+++ www/login.php
@@ -8,6 +8,14 @@
         <title>Administrador</title>
     </head>
     <body>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		ga('create', 'UA-52755902-1', 'auto');
+		ga('send', 'pageview');
+    </script>
         <div id="container" class="container">
             <div class="row">
             	<div class="col-md-1"></div>
@@ -22,20 +30,21 @@
                     <div class="row">
                         <div class="col-md-3"></div>
                         <div class="col-md-6" align="center">
-	                     <form class="flogin ensayo">
+	                     <form class="flogin ensayo" action="validar.php" method="post">
                                 <div class="form-group">
                                     <div class="input-group">
                                         <div class="input-group-addon" ><img src="iconos/usuario.png"  style="width:20px;"></div>
-                                        <input type="text" class="form-control" id="inputusuario" placeholder="Usuario">
+                                        <input type="text" class="form-control" id="inputusuario" name="inputusuario" placeholder="Usuario" required="required" autofocus="autofocus">
                                     </div>
                                 </div>
                                 <div class="form-group">
                                     <div class="input-group">
                                         <div class="input-group-addon"><img src="iconos/contraseña.png"  style="width:20px;"></div>
-                                        <input type="text" class="form-control" id="inputusuario" placeholder="Contraseña">
+                                        <input type="password" class="form-control" id="inputclave" name="inputclave" placeholder="Contraseña" required="required">
                                     </div>
+                                    <button class="btn pull-right btn-default" id="uno" name="botonlogin" value="botonlogin">Iniciar</button>
                                 </div>
-                                 <button class="btn pull-right" id="uno">Iniciar</button>
+                                 
 							</form> 
                          </div>
                         <div class="col-md-3"></div>
@@ -43,7 +52,7 @@
                 </div>
                 <div class="col-md-1"></div>
             </div>
-            <div class="row">
+            <div class="row ">
                 <div class="col-md-1"></div>
                 <div class="col-md-10 " align="center">
                     <img src="imagenes/banner4.png" class="img-responsive" />
