e1bc608d7b4915edacb9212cb7e99771a9c9a92c
obocanegra <oabs03@gmail.com>
2016-08-10 15:45:39 -0600

    Create Glosario.php
    este archivo contiene un glosario que traduce los encabezados de paginas.

---
+++ www/clases/Glosario.php
@@ -0,0 +1,195 @@
+class Glosario
+{
+function glosario()
+{
+
+}
+
+function buscar_y_remplazar($a)
+{
+$diccionario[0][0]='auditoria_historico';
+$diccionario[1][0]='caracterizacion';
+$diccionario[2][0]='clave';
+$diccionario[3][0]='cod_concepto';
+$diccionario[4][0]='cod_indicativo';
+$diccionario[5][0]='desc_adic';
+$diccionario[6][0]='desc_cargo';
+$diccionario[7][0]='desc_conceptos';
+$diccionario[8][0]='desc_correo';
+$diccionario[9][0]='desc_cta';
+$diccionario[10][0]='desc_doc';
+$diccionario[11][0]='desc_est_civil';
+$diccionario[12][0]='desc_estudio';
+$diccionario[13][0]='desc_gen';
+$diccionario[14][0]='desc_historico';
+$diccionario[15][0]='desc_prof';
+$diccionario[16][0]='desc_rubr';
+$diccionario[17][0]='desc_sit_lab';
+$diccionario[18][0]='desc_tipo_vivi';
+$diccionario[19][0]='descripcion';
+$diccionario[20][0]='dig_veri';
+$diccionario[21][0]='div_act_eco_emp';
+$diccionario[22][0]='emp_tra';
+$diccionario[23][0]='estado';
+$diccionario[24][0]='fec_cambio';
+$diccionario[25][0]='fec_corte';
+$diccionario[26][0]='fec_exp';
+$diccionario[27][0]='fec_ing_act_eco';
+$diccionario[28][0]='fec_nac';
+$diccionario[29][0]='fec_reg';
+$diccionario[30][0]='fec_trans';
+$diccionario[31][0]='fec_ven';
+$diccionario[32][0]='fecha_registro';
+$diccionario[33][0]='gru_act_eco';
+$diccionario[34][0]='id_dato_ciudad';
+$diccionario[35][0]='idem_trab';
+$diccionario[36][0]='inf_adic_d';
+$diccionario[37][0]='inf_adic_p';
+$diccionario[38][0]='nom_ciudad';
+$diccionario[39][0]='nom_com';
+$diccionario[40][0]='nom_dpto';
+$diccionario[41][0]='nom_pais';
+$diccionario[42][0]='num_cta';
+$diccionario[43][0]='num_id';
+$diccionario[44][0]='num_per_dep';
+$diccionario[45][0]='pri_ape';
+$diccionario[46][0]='pro_resi';
+$diccionario[47][0]='rol_usuario';
+$diccionario[48][0]='seg_ape';
+$diccionario[49][0]='sn_activos';
+$diccionario[50][0]='sn_fijo';
+$diccionario[51][0]='sn_oficina';
+$diccionario[52][0]='tb_datos_basicos';
+$diccionario[53][0]='tel_num';
+$diccionario[54][0]='tipo_moneda';
+$diccionario[55][0]='usuario';
+$diccionario[56][0]='usuario_historico';
+$diccionario[57][0]='vlr';
+$diccionario[58][0]='vlr_moneda';
+$diccionario[59][0]='cod_ciudad';
+$diccionario[60][0]='cod_dpto';
+$diccionario[61][0]='cod_pais';
+$diccionario[62][0]='cod_cargo';
+$diccionario[63][0]='cod_conceptos';
+$diccionario[64][0]='cod_correo';
+$diccionario[65][0]='cod_est_civil';
+$diccionario[66][0]='cod_gen';
+$diccionario[67][0]='cod_moneda';
+$diccionario[68][0]='cod_estudio';
+$diccionario[69][0]='cod_prof';
+$diccionario[70][0]='cod_rol_usuario';
+$diccionario[71][0]='cod_rubr';
+$diccionario[72][0]='cod_sit_lab';
+$diccionario[73][0]='cod_termino_sigla';
+$diccionario[74][0]='cod_tipo_cta';
+$diccionario[75][0]='cod_tipo_doc';
+$diccionario[76][0]='cod_vivi';
+$diccionario[77][0]='cod_cambio_moneda';
+$diccionario[78][0]='cod_dto_bs_conceptos';
+$diccionario[79][0]='cod_dir';
+$diccionario[80][0]='cod_historico';
+$diccionario[81][0]='cod_rol_usuario_datos_basicos';
+$diccionario[82][0]='cod_tel';
+$diccionario[83][0]='cod_tns';
+$diccionario[0][1]='Auditoria Histórico';
+$diccionario[1][1]='Caracterización';
+$diccionario[2][1]='Clave';
+$diccionario[3][1]='Código Concepto';
+$diccionario[4][1]='Indicativo';
+$diccionario[5][1]='Descripción Adicional';
+$diccionario[6][1]='Cargo';
+$diccionario[7][1]='Concepto';
+$diccionario[8][1]='Correo';
+$diccionario[9][1]='Cuenta';
+$diccionario[10][1]='Tipo Documento';
+$diccionario[11][1]='Estado civil';
+$diccionario[12][1]='Estudios';
+$diccionario[13][1]='Genero';
+$diccionario[14][1]='Histórico';
+$diccionario[15][1]='Profesión';
+$diccionario[16][1]='Rubro';
+$diccionario[17][1]='Situación Laboral';
+$diccionario[18][1]='Tipo Vivienda';
+$diccionario[19][1]='Sigla';
+$diccionario[20][1]='Digito Verificación';
+$diccionario[21][1]='División Actividad Económica Empresa';
+$diccionario[22][1]='Empresa  Donde Trabajo';
+$diccionario[23][1]='Estado';
+$diccionario[24][1]='Fecha Cambio';
+$diccionario[25][1]='Fecha Corte';
+$diccionario[26][1]='Fecha Expedición';
+$diccionario[27][1]='Fecha Ingreso Actividad Económica';
+$diccionario[28][1]='Fecha Nacimiento';
+$diccionario[29][1]='Fecha registro';
+$diccionario[30][1]='Fecha Transacción';
+$diccionario[31][1]='Fecha Vencimiento';
+$diccionario[32][1]='Fecha registro';
+$diccionario[33][1]='Grupo Actividad Económica';
+$diccionario[34][1]='Identificación Ciudad';
+$diccionario[35][1]='Identificación Trabajo';
+$diccionario[36][1]='Información Adicional Departamento';
+$diccionario[37][1]='Información Adicional País';
+$diccionario[38][1]='Ciudad';
+$diccionario[39][1]='Nombre Completo';
+$diccionario[40][1]='Departamento';
+$diccionario[41][1]='País';
+$diccionario[42][1]='Numero Cuenta';
+$diccionario[43][1]='Numero Identificación';
+$diccionario[44][1]='Numero Personas Departamento';
+$diccionario[45][1]='Primer Apellido';
+$diccionario[46][1]='Procedencia Residencia';
+$diccionario[47][1]='Rol Usuario';
+$diccionario[48][1]='Segundo Apellido';
+$diccionario[49][1]='Activos';
+$diccionario[50][1]='Numero Fijo';
+$diccionario[51][1]='Numero Oficina';
+$diccionario[52][1]='Datos Básicos';
+$diccionario[53][1]='Numero Teléfono';
+$diccionario[54][1]='Moneda';
+$diccionario[55][1]='Usuario';
+$diccionario[56][1]='Usuario Histórico';
+$diccionario[57][1]='Valor';
+$diccionario[58][1]='Valor Moneda';
+$diccionario[59][1]='código ciudad ';
+$diccionario[60][1]='código departamento ';
+$diccionario[61][1]='código país ';
+$diccionario[62][1]='código cargo ';
+$diccionario[63][1]='código concepto ';
+$diccionario[64][1]='código correo';
+$diccionario[65][1]='código estado civil ';
+$diccionario[66][1]='código genero ';
+$diccionario[67][1]='código moneda ';
+$diccionario[68][1]='código estudio ';
+$diccionario[69][1]='código profesión';
+$diccionario[70][1]='código rol usuario';
+$diccionario[71][1]='código rubros ';
+$diccionario[72][1]='código situación laboral ';
+$diccionario[73][1]='código termino sigla';
+$diccionario[74][1]='código tipo cuota ';
+$diccionario[75][1]='código tipo documento ';
+$diccionario[76][1]='código vivienda ';
+$diccionario[77][1]='código cambio moneda ';
+$diccionario[78][1]='código datos básicos concepto';
+$diccionario[79][1]='código dirección ';
+$diccionario[80][1]='código historia ';
+$diccionario[81][1]='código rol usuario datos básicos';
+$diccionario[82][1]='código teléfono ';
+$diccionario[83][1]='código transacciones ';
+
+
+	//$a='fec_ven';
+	$salida="";
+	for($i=0;$i<count($diccionario);$i++)
+	{
+		if($diccionario[$i][0]==$a)
+		{
+			for($j=0;$j<count($diccionario[$i]);$j++)
+			{
+			 $salida.=$diccionario[$i][1];
+			  break;
+			}	
+		}
+	}
+	return $salida;
+}
+}
